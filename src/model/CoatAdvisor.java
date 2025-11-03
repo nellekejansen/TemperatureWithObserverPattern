9cb28c2dfd4fb5ae0e56790baf39ccb4d75b6e6a
Nelleke <nellekejansen96@gmail.com>
2025-11-03 13:20:09 +0100

    Implemented CoatAdvisor

--- src/model/CoatAdvisor.java
+++ src/model/CoatAdvisor.java
@@ -2,7 +2,26 @@ package model;
 
 /**
  * @author Nelleke Jansen
- * Purpose for the class
+ * Gives advice about whether you need a coat, based on the temperature.
  */
-public class CoatAdvisor {
+public class CoatAdvisor implements Observer {
+    private double temperature;
+
+    @Override
+    public void update(double temperature) {
+        this.temperature = temperature;
+        display();
+    }
+
+    private void display() {
+        if (this.temperature < 5) {
+            System.out.println("Wear a warm coat, it's super cold!");
+        } else if (this.temperature < 15) {
+            System.out.println("Wear a coat.");
+        } else if (this.temperature < 20) {
+            System.out.println("Wear a light jacket");
+        } else {
+            System.out.println("No coat necessary, it's warm outside!");
+        }
+    }
 }
