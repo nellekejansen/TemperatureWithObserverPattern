902e8f8dd3840dc3837bf4123b63dad41c11f963
Nelleke <nellekejansen96@gmail.com>
2025-11-03 13:09:41 +0100

    Implemented WeatherStation

--- src/model/WeatherStation.java
+++ src/model/WeatherStation.java
@@ -1,8 +1,40 @@
 package model;
 
+import java.util.ArrayList;
+
 /**
  * @author Nelleke Jansen
  * A place where the temperature is measured.
  */
-public class WeatherStation {
+public class WeatherStation implements Observable {
+    public static final double DEFAULT_TEMPERATURE = 0.0;
+    private ArrayList<Observer> observers;
+    private double temperature;
+
+    public WeatherStation() {
+        this.observers = new ArrayList<>();
+        this.temperature = DEFAULT_TEMPERATURE;
+    }
+
+    @Override
+    public void addObserver(Observer observer) {
+        observers.add(observer);
+    }
+
+    @Override
+    public void removeObserver(Observer observer) {
+        observers.remove(observer);
+    }
+
+    @Override
+    public void notifyObservers() {
+        for (Observer observer : observers) {
+            observer.update(temperature);
+        }
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+        notifyObservers();
+    }
 }
