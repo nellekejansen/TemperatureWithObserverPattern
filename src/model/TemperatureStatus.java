cfd07fc6fa73953035cd40649d2c6448bab69d2d
Josse Muller <jossemuller@live.nl>
2025-11-03 13:19:26 +0100

    Added controller and TemperatureStatus Observer

--- src/model/TemperatureStatus.java
+++ src/model/TemperatureStatus.java
@@ -1,8 +1,19 @@
 package model;
 
 /**
- * @author Nelleke Jansen
- * Purpose for the class
+ * Display the temperature in a string with one decimal
+ * @author Josse Muller
  */
-public class TemperatureStatus {
+public class TemperatureStatus implements Observer {
+    private double temperature;
+
+    @Override
+    public void update(double temperature) {
+        this.temperature = temperature;
+        display();
+    }
+
+    public void display() {
+        System.out.printf("De temperatuur is nu: %.1f\n", this.temperature);
+    }
 }
